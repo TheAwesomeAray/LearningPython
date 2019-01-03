bd4b53ccbad4ebd61d1bf7c8a91df20276b1fec8
TheAwesomeARay <andrewray4@gmail.com>
2019-01-02 20:50:17 -0600

    Exception handling

--- Exceptions.py
+++ Exceptions.py
@@ -1,1 +1,25 @@
 
+student = {
+    "name": "Andrew",
+    "student_id": 1,
+    "grade": "A"
+}
+
+try:
+    last_name = student["last_name"]
+except KeyError:
+    print("Error! Could not find last name.")
+
+
+student["last_name"] = "Ray"
+
+try:
+    last_name = student["last_name"]
+    print(last_name)
+    numbered_last_name = 3 + last_name
+except KeyError:
+    print("Error! Could not find last name.")
+#except TypeError:
+    #print("Error! Cannot add these types.")
+except Exception:
+    print("Unknown Error!")
