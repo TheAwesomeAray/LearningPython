e9baab409eaf03bc076fcd11f1e0a2e0d3debeb8
TheAwesomeARay <andrewray4@gmail.com>
2019-01-04 18:54:50 -0600

    Reading and Writing to files

--- StudentManager/Students.py
+++ StudentManager/Students.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = f"{student['name']}  {student['student_id']}"
+        students_titlecase.append(student["name"])
     return students_titlecase
 
 def print_students_titlecase():
@@ -13,9 +13,27 @@ def add_student(name, student_id = 0):
     student = { "name": name, "student_id": student_id }
     students.append(student)
 
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception as error:
+        print("Failed to save student")
+        print(error)
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+read_file()
 student_name = input("Please enter the student's name: ")
-student_id = input("Please enter the student's id: ")
+add_student(student_name)
 
-add_student(student_name, student_id)
-
+save_file(student_name)
 print_students_titlecase()
